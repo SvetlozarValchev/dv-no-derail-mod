388bbd786f249dbefd5cb78a97b580c1126217ec
Svetlozar Valchev <cbethax@gmail.com>
2019-03-07 22:39:51 +0200

    initial

---
+++ NoDerailMod/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using UnityModManagerNet;
+using Harmony12;
+using UnityEngine;
+
+namespace NoDerailMod
+{
+    public class Main
+    {
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            // Something
+            return true; // If false the mod will show an error.
+        }
+    }
+
+    [HarmonyPatch(typeof(Bogie), "Start")]
+    class Bogie_Start_Patch
+    {
+        static void Postfix(ref bool ___canDerail)
+        {
+            ___canDerail = false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# dv-no-derail-mod
